8dc439531082dcb187c23afd29366fef8c023b76
devarchi33 <devarchi33@gmail.com>
2016-05-10 00:47:43 +0900

    1. seminar 객체 내부에 task 리스트 포함 형태로 구조 변경.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,23 +8,28 @@ angular.module('DomainDrivenApp', [])
     .controller('MainCtrl', function ($scope) {
         $scope.menus = [
             {
+                id: 0,
                 name: "Ready for seminar app",
                 url: "index.html"
             },
             {
+                id: 1,
                 name: "Now",
                 url: "#/app/now",
                 class: "active"
             },
             {
+                id: 2,
                 name: "Archive",
                 url: "#/app/archive"
             },
             {
+                id: 3,
                 name: "Register Speaker",
                 url: "#/app/register"
             },
             {
+                id: 4,
                 name: "Logout",
                 url: "#/login"
             }
@@ -33,41 +38,196 @@ angular.module('DomainDrivenApp', [])
 
         $scope.seminars = [
             {
+                id: 0,
                 title: "제 1회 정기 세미나",
-                state: "Archived"
+                state: "Archived",
+                tasks: [
+                    {
+                        id: 0,
+                        name: "강사 섭외",
+                        worker: "안재열",
+                        progress: "100",
+                        taskDetail: "speaker.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 1,
+                        name: "장소 예약",
+                        worker: "이동훈",
+                        progress: "100",
+                        taskDetail: "place.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 2,
+                        name: "포스터 제작",
+                        worker: "안재열",
+                        progress: "100",
+                        taskDetail: "poster.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 3,
+                        name: "모임 등록",
+                        worker: "박세종",
+                        progress: "100",
+                        taskDetail: "seminar.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 4,
+                        name: "홍보",
+                        worker: ["박세종", "안재열", "이동훈"],
+                        progress: "100",
+                        taskDetail: "promote.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 5,
+                        name: "회고",
+                        worker: ["박세종", "안재열", "이동훈"],
+                        progress: "100",
+                        taskDetail: "rememberence.html",
+                        workerDetail: ""
+                    }
+                ]
             },
             {
+                id: 1,
                 title: "제 2회 정기 세미나",
-                state: "Archived"
+                state: "Archived",
+                tasks: [
+                    {
+                        id: 0,
+                        name: "강사 섭외",
+                        worker: "안재열",
+                        progress: "100",
+                        taskDetail: "speaker.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 1,
+                        name: "장소 예약",
+                        worker: "이동훈",
+                        progress: "100",
+                        taskDetail: "place.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 2,
+                        name: "포스터 제작",
+                        worker: "안재열",
+                        progress: "100",
+                        taskDetail: "poster.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 3,
+                        name: "모임 등록",
+                        worker: "박세종",
+                        progress: "100",
+                        taskDetail: "seminar.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 4,
+                        name: "홍보",
+                        worker: ["박세종", "안재열", "이동훈"],
+                        progress: "100",
+                        taskDetail: "promote.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 5,
+                        name: "회고",
+                        worker: ["박세종", "안재열", "이동훈"],
+                        progress: "100",
+                        taskDetail: "rememberence.html",
+                        workerDetail: ""
+                    }
+                ]
             },
             {
+                id: 2,
                 title: "제 3회 정기 세미나",
                 state: "Now",
-                class: "active"
+                class: "active",
+                tasks: [
+                    {
+                        id: 0,
+                        name: "강사 섭외",
+                        worker: "안재열",
+                        progress: "100",
+                        taskDetail: "speaker.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 1,
+                        name: "장소 예약",
+                        worker: "이동훈",
+                        progress: "100",
+                        taskDetail: "place.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 2,
+                        name: "포스터 제작",
+                        worker: "안재열",
+                        progress: "100",
+                        taskDetail: "poster.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 3,
+                        name: "모임 등록",
+                        worker: "박세종",
+                        progress: "100",
+                        taskDetail: "seminar.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 4,
+                        name: "홍보",
+                        worker: ["박세종", "안재열", "이동훈"],
+                        progress: "100",
+                        taskDetail: "promote.html",
+                        workerDetail: ""
+                    },
+                    {
+                        id: 5,
+                        name: "회고",
+                        worker: ["박세종", "안재열", "이동훈"],
+                        progress: "0",
+                        taskDetail: "rememberence.html",
+                        workerDetail: ""
+                    }
+                ]
             }
         ];
 
-        $scope.tasks = [
-            {
-                name: "강사 섭외",
-                worker: "안재열",
-                progress: "80",
-                taskDetail: "speaker.html",
-                workerDetail: ""
-            },
-            {
-                name: "장소 예약",
-                worker: "이동훈",
-                progress: "50",
-                taskDetail: "place.html",
-                workerDetail: ""
-            },
-            {
-                name: "모임 등록",
-                worker: " 박세종",
-                progress: "0",
-                taskDetail: "seminar.html",
-                workerDetail: ""
-            }
-        ];
+        $scope.currentMenu = $scope.menus[1];
+        $scope.currentSeminar = $scope.seminars[$scope.seminars.length - 1];
+
+        function setCurrentMenu(menu) {
+            $scope.currentMenu = menu;
+            console.log("Current Menu: " + menu.name);
+        }
+
+        function isCurrentMenu(menu) {
+            return $scope.currentMenu !== null && menu.name === $scope.currentMenu.name;
+        }
+
+        function setCurrentSeminar(seminar) {
+            $scope.currentSeminar = seminar;
+            console.log("Current Seminar: " + seminar.title);
+        }
+
+        function isCurrentSeminar(seminar) {
+            return $scope.currentSeminar !== null && seminar.title === $scope.currentSeminar.title;
+        }
+
+        $scope.setCurrentMenu = setCurrentMenu;
+        $scope.isCurrentMenu = isCurrentMenu;
+        $scope.setCurrentSeminar = setCurrentSeminar;
+        $scope.isCurrentSeminar = isCurrentSeminar;
     });
